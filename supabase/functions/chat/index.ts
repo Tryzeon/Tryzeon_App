8c5591fc42311983af744f107327000a5c2ced5f
erichung9060 <rthung96@gmail.com>
2025-11-23 21:53:37 +0800

    fix: remove keep-alive header

--- supabase/functions/chat/index.ts
+++ supabase/functions/chat/index.ts
@@ -2,14 +2,14 @@ import "jsr:@supabase/functions-js/edge-runtime.d.ts";
 import { GoogleGenerativeAI } from "npm:@google/generative-ai";
 
 Deno.serve(async (req) => {
-  const genAI = new GoogleGenerativeAI(Deno.env.get("API_KEY2"));
+  const genAI = new GoogleGenerativeAI(Deno.env.get("API_KEY"));
   const LLM_Model = "gemini-2.5-flash";
 
   const { userRequirement } = await req.json();
-  
+
   var prompt = `請根據以下穿搭需求，提供具體的服裝搭配建議，包括上衣、下身、鞋子和配件的推薦，簡短一點即可，但要分段說明。
   ${userRequirement}`;
-  
+
   const model = genAI.getGenerativeModel({
     model: LLM_Model
   });
@@ -21,8 +21,7 @@ Deno.serve(async (req) => {
     text
   }), {
     headers: {
-      "Content-Type": "application/json",
-      "Connection": "keep-alive"
+      "Content-Type": "application/json"
     }
   });
 });
