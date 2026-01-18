1b5674b73a0c41a148442adc86dac438c9f6011b
erichung9060 <rthung96@gmail.com>
2026-01-18 12:13:48 +0800

    feat: integrate Isar database for local data persistence.

---
+++ lib/core/services/isar_service.dart
@@ -0,0 +1,38 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:tryzeon/feature/auth/data/collections/auth_settings_collection.dart';
+import 'package:tryzeon/feature/common/product_type/data/collections/product_type_collection.dart';
+import 'package:tryzeon/feature/personal/profile/data/collections/user_profile_collection.dart';
+import 'package:tryzeon/feature/personal/wardrobe/data/collections/wardrobe_item_collection.dart';
+import 'package:tryzeon/feature/store/products/data/collections/product_collection.dart';
+import 'package:tryzeon/feature/store/profile/data/collections/store_profile_collection.dart';
+
+final isarServiceProvider = Provider<IsarService>((final ref) => IsarService());
+
+class IsarService {
+  IsarService() {
+    db = openDB();
+  }
+  late Future<Isar> db;
+
+  Future<Isar> openDB() async {
+    if (Isar.instanceNames.isEmpty) {
+      final dir = await getApplicationDocumentsDirectory();
+      return Isar.open(
+        [
+          AuthSettingsCollectionSchema,
+          ProductTypeCollectionSchema,
+          UserProfileCollectionSchema,
+          WardrobeItemCollectionSchema,
+          ProductCollectionSchema,
+          StoreProfileCollectionSchema,
+        ],
+        directory: dir.path,
+        inspector: true,
+      );
+    }
+
+    return Future.value(Isar.getInstance());
+  }
+}
