0baca893fb4d696726aec46d510f9249324587c0
erichung <rthung96@gmail.com>
2026-06-15 02:26:01 +0800

    chore(release): remove uncommitted changes check from beta release script

--- scripts/release-beta.sh
+++ scripts/release-beta.sh
@@ -31,12 +31,6 @@ if [[ ! "$version" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
   exit 1
 fi
 
-# Check for uncommitted changes
-if [[ -n $(git status --porcelain) ]]; then
-  echo "Error: You have uncommitted changes. Please commit or stash them first." >&2
-  exit 1
-fi
-
 # Check for unpushed commits
 if [[ -n $(git log origin/$(git branch --show-current)..HEAD) ]]; then
   echo "Error: You have unpushed commits" >&2
