0d8dbc76cea48b428dc391a4708a9cbb39848f8a
erichung <rthung96@gmail.com>
2026-06-28 02:17:42 +0800

    feat(tryon): add tap-to-close gesture to fullscreen viewer

--- lib/feature/personal/home/presentation/pages/tryon_fullscreen_viewer.dart
+++ lib/feature/personal/home/presentation/pages/tryon_fullscreen_viewer.dart
@@ -64,6 +64,8 @@ class _ViewerLayer extends HookWidget {
                 initialScale: PhotoViewComputedScale.covered,
                 minScale: PhotoViewComputedScale.covered,
                 maxScale: PhotoViewComputedScale.covered * 3,
+                onTapUp: (final context, final details, final controllerValue) =>
+                    close(),
                 loadingBuilder: (final context, final event) =>
                     const Center(child: CircularProgressIndicator()),
               ),
