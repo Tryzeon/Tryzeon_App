82abbc46510ff6d04c58f50e64256fe8b13bbcd3
erichung9060 <rthung96@gmail.com>
2026-08-04 15:23:13 +0800

    refactor(ad): update ad image sources

--- lib/feature/personal/shop/data/datasources/ad_local_datasource.dart
+++ lib/feature/personal/shop/data/datasources/ad_local_datasource.dart
@@ -2,11 +2,14 @@ class AdLocalDataSource {
   Future<List<String>> getAdImages({final bool forceRefresh = false}) async {
     // In a real app, logic for forceRefresh would be here (e.g., clearing local cache)
     return [
-      'assets/images/ads/brand1.jpg',
-      'assets/images/ads/brand2.jpg',
-      'assets/images/ads/brand3.jpg',
-      'assets/images/ads/brand4.jpg',
-      'assets/images/ads/brand5.jpg',
+      'assets/images/ads/1.jpg',
+      'assets/images/ads/2.jpg',
+      'assets/images/ads/3.jpg',
+      'assets/images/ads/4.jpg',
+      'assets/images/ads/5.jpg',
+      'assets/images/ads/6.jpg',
+      'assets/images/ads/7.jpg',
+      'assets/images/ads/8.jpg',
     ];
   }
 }
